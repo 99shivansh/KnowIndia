969f73d3bc8965882f356771f4782ad7eceabb39
Shivansh Sharma <shivanshsharma@Shivanshs-MacBook-Air.local>
2023-05-14 15:52:28 +0530

    update

--- public/polyfills-es5.js
+++ public/polyfills-es5.js
@@ -18899,7 +18899,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! /home/shivansh/Desktop/knowindiav3/angular-src/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
+    /*! /Users/shivanshsharma/Desktop/KnowIndia/angular-src/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -18907,7 +18907,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! /home/shivansh/Desktop/knowindiav3/angular-src/src/polyfills.ts */
+    /*! /Users/shivanshsharma/Desktop/KnowIndia/angular-src/src/polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
